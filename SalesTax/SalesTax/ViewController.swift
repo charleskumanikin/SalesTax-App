4feaadff371d273c51fdc3d7354a6b2e4e6966af
Adrian <81994521+charleskumanikin@users.noreply.github.com>
2021-11-17 21:39:25 +0200

    Add files via upload

---
+++ SalesTax/SalesTax/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SalesTax
+//
+//  Created by Adrian Kumanikin on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var priceTxt: UITextField!
+    @IBOutlet weak var salesTaxTxt: UITextField!
+    @IBOutlet weak var totalPriceLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        totalPriceLbl.text = ""
+    
+    }
+
+    @IBAction func calculateTotalPrice(_ sender: Any) {
+        let price = Double(priceTxt.text!)!
+        let salesTax = Double(salesTaxTxt.text!)!
+        
+        let totalSalesTax = price * salesTax
+        let totalPrice = price + totalSalesTax
+        totalPriceLbl.text = "$\(totalPrice)"
+    }
+    
+}
+
